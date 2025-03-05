89729881f35d809a4b69a708b2f10a83934dedc5
DannyJ07 <djyanchapanta@espe.edu.ec>
2025-03-05 17:46:59 -0500

    Puerto 000 para render

--- back/app.py
+++ back/app.py
@@ -56,5 +56,4 @@ def notificacion():
     return jsonify({'mensaje': f'Tienes {len(pendientes)} facturas pendientes'})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
-
+    app.run(debug=True, host='0.0.0.0', port=5000)
